580ab1db49d233af2af0e2b1a6ab2aa02411227e
Aymen Mehri <med.aymen.mehri@gmail.com>
2018-04-05 00:40:54 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { AreaChartComponent } from './area-chart/area-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bar', component: BarChartComponent },
+  { path: 'area', component: AreaChartComponent },
+  { path: 'pie', component: PieChartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
